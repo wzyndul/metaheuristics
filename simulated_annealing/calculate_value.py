d0bb16cecbe977b5b3d15319e5a829b0f5c7d079
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-11-01 21:58:53 +0100

    Changed neighbours function, trying to calculate tick

--- simulated_annealing/calculate_value.py
+++ simulated_annealing/calculate_value.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 
-def function_value(x, function_type): # na przedziale [-150, 150]
+# Obliczanie wartości funkcji w punkcie x
+def function_value(x, function_type):
     fx = 0
-    # - 2 |x + 100| + 10 dla (-105, -95)
-    # -2.2 |x - 100| + 11 dla (95, 105)
-    # 0 dla x != (-105, -95) lub (95, 105)
+    # na przedziale [-150, 150]
+    # - 2 |x + 100| + 10     dla (-105, -95)
+    # -2.2 |x - 100| + 11    dla (95, 105)
+    # 0                      dla x != (-105, -95) lub (95, 105)
     if function_type == "1":
         if -105 < x < -95:
             fx = -2 * np.abs(x + 100) + 10
@@ -14,8 +16,11 @@ def function_value(x, function_type): # na przedziale [-150, 150]
         else:
             fx = 0
 
-    elif function_type == "2":  # f(x) = x sin(10PI x) + 1, dla [-1, 2]
+    # na przedziale [-1, 2]
+    # f(x) = x sin(10PI x) + 1
+    elif function_type == "2":
         fx = x * np.sin(10 * np.pi * x) + 1
+
     else:
         print("Podano błędny numer funkcji")
     return fx
--- simulated_annealing/main.py
+++ simulated_annealing/main.py
@@ -3,21 +3,34 @@ import random
 import calculate_value as cal
 import sys
 
-# temperatura, wartosc przy alfa(t), k , M, przedzial, ktora funkcja
-T = float(sys.argv[1])
-ALFA_T = float(sys.argv[2])
-K = float(sys.argv[3])
-M = int(sys.argv[4])
-LEFT = float(sys.argv[5])
-RIGHT = float(sys.argv[6])
-FUNC = sys.argv[7]
+# Wczytywanie parametrów z linii poleceń
+T = float(sys.argv[1])          # temperatura początkowa
+ALFA_T = float(sys.argv[2])     # współczynnik zmiany temperatury
+K = float(sys.argv[3])          # stała Boltzmanna
+M = int(sys.argv[4])            # liczba iteracji
+LEFT = float(sys.argv[5])       # lewa granica przedziału
+RIGHT = float(sys.argv[6])      # prawa granica przedziału
+FUNC = sys.argv[7]              # numer funkcji
+
+# def random_neighbour(x, tick):
+#     change = random.uniform(-tick, tick)
+#     if x + change > RIGHT or x + change < LEFT:
+#         return x - change
+#     else:
+#         return x + change
+
+
 def random_neighbour(x, tick):
     change = random.uniform(-tick, tick)
-    if x + change > RIGHT or x + change < LEFT:
-        return x - change
-    else:
-        return x + change
+    x_neighbor = x + change
 
+    # Sprawdzanie czy punkt nie wychodzi poza przedział
+    x_neighbor = max(min(x_neighbor, RIGHT), LEFT)
+    return x_neighbor
+
+
+def calculate_tick(initial_temperature, final_temperature, iterations):
+    return (initial_temperature - final_temperature) / iterations
 
 
 # 1. inicjalizacja początkowej wartości
@@ -33,6 +46,7 @@ x_next = 0
 
 delta_cost = 0
 x_best = x_value
+curr_T = T
 
 for i in range(0, M):
     x_next = random_neighbour(x_value, 15)
