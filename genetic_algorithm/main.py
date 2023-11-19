2253e9c34c7f8e2b4ba545cb13a89eed6d030729
wzyndul <wojteckiz8630@gmail.com>
2023-11-19 22:37:09 +0100

    [IMPLEMENTED] individual mutation, started tournament selection

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -1,9 +1,11 @@
 import random
 
-BACKPACK = [(1,3), (2,4)] # (weight, cost)
+BACKPACK = [(1, 3), (2, 4)]  # (weight, cost)
 MAX_WEIGHT = 9
-def calculate_adaptation(individual): # backacp i max weight jako stałe jakies zrobie na razie
-    weight = 0                                              # najwyzej sie to obiektowo potem zrobi
+
+
+def calculate_adaptation(individual):  # backacp i max weight jako stałe jakies zrobie na razie
+    weight = 0  # najwyzej sie to obiektowo potem zrobi
     cost = 0
     for index, value in individual:
         if value == 1:
@@ -31,7 +33,7 @@ def set_probability_in_population(population):
     for individual in population:
         adaptation_sum += calculate_adaptation(individual)
     for individual in population:
-        probability.append(calculate_adaptation(individual)/adaptation_sum)
+        probability.append(calculate_adaptation(individual) / adaptation_sum)
     return probability
 
 
@@ -54,21 +56,32 @@ def roulette_selection(population, number_of_parents):
             if section[1] < random_numer <= section[2]:
                 chosen_individuals.append(population.pop([section[0]]))
                 # robie popa, zeby usunac z popualcji tego osobniak, zeby przypadkiem nei wybrac go dwa razy w metodzie ruletki
-                #TODO still nie wiem do konca jak chce zrobic to wybieranie rodzicow w sensie ilu ich itd
+                # TODO still nie wiem do konca jak chce zrobic to wybieranie rodzicow w sensie ilu ich itd
                 # TODO ale chyba po prostu krzyzowanie i powstaje po 2 dzieci i gitara jest
 
                 break
 
     return chosen_individuals
 
-#todo pamietaj o tym!!!!
+def tournament_selection(group_size, population): #TODO jakies zabezpieczenie zeby nie bylo jakis ulamków czy cos(?)
+    random.shuffle(population)
+    parents = []
+    to_remove = []
+    tmp = []
+    for index, value in population:
+        pass
+
+
+
+# todo pamietaj o tym!!!!
 # Jeśli prawdopodobieństwo krzyżowania wynosi 1.0, to krzyżowanie będzie miało miejsce za każdym razem podczas
 # tworzenia potomstwa. Z drugiej strony, jeśli prawdopodobieństwo krzyżowania wynosi 0.0,
 # to krzyżowanie nie będzie miało miejsca, a potomstwo będzie identyczne z rodzicami.
 def single_point_crossing(parent_a, parent_b, crossover_probability):
     if random.random() < crossover_probability:
         children_a, children_b = [], []
-        crossing_point = random.randint(1, 7) # wykluczamy wybranie wszystkich genów z 1 rodzica, 7 bo i tak jest to excluded w rangu
+        crossing_point = random.randint(1,
+                                        7)  # wykluczamy wybranie wszystkich genów z 1 rodzica, 7 bo i tak jest to excluded w rangu
         for index in range(0, crossing_point):
             children_a.append(parent_a[index])
             children_b.append(parent_b[index])
@@ -77,7 +90,7 @@ def single_point_crossing(parent_a, parent_b, crossover_probability):
             children_b.append(parent_a[index])
         return children_a, children_b
     else:
-        return parent_a, parent_b # dzieci są takie same jak rodzice
+        return parent_a, parent_b  # dzieci są takie same jak rodzice
 
 
 def two_point_crossing(parent_a, parent_b, crossover_probability):
@@ -86,7 +99,8 @@ def two_point_crossing(parent_a, parent_b, crossover_probability):
         while True:
             number1 = random.randint(1, 7)
             number2 = random.randint(1, 7)
-            if number1 != number2 and abs(number1 - number2) > 2: # liczby nie mogą być identyczne ani 2 miejsca od siebie (bo excluded) weic de facto 1 miejsce
+            if number1 != number2 and abs(
+                    number1 - number2) > 2:  # liczby nie mogą być identyczne ani 2 miejsca od siebie (bo excluded) weic de facto 1 miejsce
                 break
         for index in range(0, min(number1, number2)):
             children_a.append(parent_a[index])
@@ -104,7 +118,8 @@ def two_point_crossing(parent_a, parent_b, crossover_probability):
     else:
         return parent_a, parent_b  # dzieci są takie same jak rodzice
 
-def individual_mutation():
-    pass
 
-
+def individual_mutation(individual):
+    random_bit = random.randint(0, len(individual))
+    individual[random_bit] = 1 if individual[random_bit] == 0 else 0
+    return individual
