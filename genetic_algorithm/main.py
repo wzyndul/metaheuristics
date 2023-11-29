c134b5116dc2a26ee47f90252b91137df31cba0b
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-11-29 21:37:11 +0100

    Added plots

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -1,4 +1,5 @@
 from population import Population
+import matplotlib.pyplot as plt
 import sys
 
 NR_OF_EPOCHS = int(sys.argv[1])
@@ -8,9 +9,9 @@ MUTATION_PROBABILITY = float(sys.argv[4])
 
 best_values = []
 population = Population(POPULATION_SIZE, 6404180)
-best_individual = 0
-population.generate_individuals()
 for y in range(5):
+    best_individual = 0
+    population.generate_individuals()
     for x in range(NR_OF_EPOCHS):
         population.calculate_adaptation()
         best_individual = population.get_best_individual()
@@ -20,4 +21,12 @@ for y in range(5):
         population.choose_parents()
         population.generate_children(CROSSOVER_PROBABILITY, MUTATION_PROBABILITY)
 
-best_values.append(best_individual.get_cost())
+    best_values.append(best_individual.get_cost())
+
+plt.plot(list(range(1, 6)), best_values, marker='o')
+plt.xlabel('Run')
+plt.ylabel('Best individual value')
+plt.title('Best individual values for 5 runs')
+plt.xticks(list(range(1,6)))
+plt.grid(True)
+plt.show()
