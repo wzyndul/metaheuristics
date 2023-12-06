e5e4bce11d303080f09da43d53a78841b3976eb9
wzyndul <wojteckiz8630@gmail.com>
2023-12-06 12:00:15 +0100

    [ADDED] start project

--- genetic_algorithm/individual.py
+++ genetic_algorithm/individual.py
@@ -3,53 +3,53 @@ import random
 
 class Individual:
     def __init__(self, size, max_weight, backpack):
-        self.__size = size
-        self.__bits = [random.choice([0, 1]) for _ in range(self.__size)]
-        self.__probability = 0  # probability of choosing this particular individual
-        self.__weight = 0
-        self.__max_weight = max_weight
-        self.__cost = 0
+        self.size = size
+        self.bits = [random.choice([0, 1]) for _ in range(self.size)]
+        self.probability = 0  # probability of choosing this particular individual
+        self.weight = 0
+        self.max_weight = max_weight
+        self.cost = 0
         self.backpack = backpack
         self.calculate_weight()
-        while self.__weight > self.__max_weight: # generate new values if backpack is too heavy
-            self.__bits = [random.choice([0, 1]) for _ in range(self.__size)]
+        while self.weight > self.max_weight: # generate new values if backpack is too heavy
+            self.bits = [random.choice([0, 1]) for _ in range(self.size)]
             self.calculate_weight()
 
     def get_bits(self):
-        return self.__bits
+        return self.bits
 
     def get_probability(self):
-        return self.__probability
+        return self.probability
 
     def get_weight(self):
-        return self.__weight
+        return self.weight
 
     def get_cost(self):
-        return self.__cost
+        return self.cost
 
     def set_bit(self, index, value):
-        self.__bits[index] = value
+        self.bits[index] = value
 
     def calculate_adaptation(self):
-        self.__weight = 0
-        self.__cost = 0
-        for index, bit in enumerate(self.__bits):
-            self.__weight += self.backpack["items"][index]["weight"] * bit
-            self.__cost += self.backpack["items"][index]["value"] * bit
-        if self.__weight > self.__max_weight:
-            self.__cost = 0  # set adaptation value (cost) to zero if backpack to heavy
-        return self.__cost
+        self.weight = 0
+        self.cost = 0
+        for index, bit in enumerate(self.bits):
+            self.weight += self.backpack["items"][index]["weight"] * bit
+            self.cost += self.backpack["items"][index]["value"] * bit
+        if self.weight > self.max_weight:
+            self.cost = 0  # set adaptation value (cost) to zero if backpack to heavy
+        return self.cost
 
     def set_probability(self, adaptation_sum):
-        self.__probability = self.__cost / adaptation_sum
+        self.probability = self.cost / adaptation_sum
 
     def mutation(self):
-        random_bit = random.randint(0, self.__size - 1)
-        self.__bits[random_bit] = 1 if self.__bits[random_bit] == 0 else 0
+        random_bit = random.randint(0, self.size - 1)
+        self.bits[random_bit] = 1 if self.bits[random_bit] == 0 else 0
 
     def calculate_weight(self):
-        self.__weight = 0
-        for index, bit in enumerate(self.__bits):
-            self.__weight += self.backpack["items"][index]["weight"] * bit
+        self.weight = 0
+        for index, bit in enumerate(self.bits):
+            self.weight += self.backpack["items"][index]["weight"] * bit
 
 
--- genetic_algorithm/population.py
+++ genetic_algorithm/population.py
@@ -6,58 +6,58 @@ from individual import Individual
 
 class Population:
     def __init__(self, size, max_weight):
-        self.__individuals = []
-        self.__max_weight = max_weight
-        self.__size = size
-        self.__adaptation_sum = 0
-        self.__parents = []
-        self.__children = []
+        self.individuals = []
+        self.max_weight = max_weight
+        self.size = size
+        self.adaptation_sum = 0
+        self.parents = []
+        self.children = []
 
         # Read data from the JSON file
         with open('data.json', 'r') as file:
             self.backpack = json.load(file)
-            self.__backpack_size = len(self.backpack["items"])
+            self.backpack_size = len(self.backpack["items"])
 
     def calculate_adaptation(self):
-        self.__adaptation_sum = 0
-        for individual in self.__individuals:
-            self.__adaptation_sum += individual.calculate_adaptation()
+        self.adaptation_sum = 0
+        for individual in self.individuals:
+            self.adaptation_sum += individual.calculate_adaptation()
 
     def set_probability_in_population(self):
-        for individual in self.__individuals:
-            individual.set_probability(self.__adaptation_sum)
+        for individual in self.individuals:
+            individual.set_probability(self.adaptation_sum)
 
     def get_best_individual(self):
-        return max(self.__individuals, key=lambda individual: individual.get_cost())
+        return max(self.individuals, key=lambda individual: individual.get_cost())
 
     def remove_individual(self, individual):
-        self.__individuals.remove(individual)
+        self.individuals.remove(individual)
 
     def generate_individuals(self):
-        for _ in range(self.__size):
-            self.__individuals.append(Individual(self.__backpack_size, self.__max_weight, self.backpack))
+        for _ in range(self.size):
+            self.individuals.append(Individual(self.backpack_size, self.max_weight, self.backpack))
 
     def single_point_crossing(self, parent_a, parent_b):
-        children_a = Individual(self.__backpack_size, self.__max_weight, self.backpack)
-        children_b = Individual(self.__backpack_size, self.__max_weight, self.backpack)
-        crossing_point = random.randint(1, self.__backpack_size - 1)
+        children_a = Individual(self.backpack_size, self.max_weight, self.backpack)
+        children_b = Individual(self.backpack_size, self.max_weight, self.backpack)
+        crossing_point = random.randint(1, self.backpack_size - 1)
 
         for index in range(0, crossing_point):
             children_a.set_bit(index, parent_a.get_bits()[index])
             children_b.set_bit(index, parent_b.get_bits()[index])
 
-        for index in range(crossing_point, self.__backpack_size):
+        for index in range(crossing_point, self.backpack_size):
             children_a.set_bit(index, parent_b.get_bits()[index])
             children_b.set_bit(index, parent_a.get_bits()[index])
         return children_a, children_b
 
     def two_point_crossing(self, parent_a, parent_b):
 
-        children_a = Individual(self.__backpack_size, self.__max_weight, self.backpack)
-        children_b = Individual(self.__backpack_size, self.__max_weight, self.backpack)
+        children_a = Individual(self.backpack_size, self.max_weight, self.backpack)
+        children_b = Individual(self.backpack_size, self.max_weight, self.backpack)
         while True:
-            first_crossing_point = random.randint(1, self.__backpack_size - 1)
-            second_crossing_point = random.randint(1, self.__backpack_size - 1)
+            first_crossing_point = random.randint(1, self.backpack_size - 1)
+            second_crossing_point = random.randint(1, self.backpack_size - 1)
             if first_crossing_point != second_crossing_point and abs(first_crossing_point - second_crossing_point) >= 1:
                 break
 
@@ -70,13 +70,13 @@ class Population:
             children_a.set_bit(index, parent_b.get_bits()[index])
             children_b.set_bit(index, parent_a.get_bits()[index])
 
-        for index in range(max(first_crossing_point, second_crossing_point), self.__backpack_size):
+        for index in range(max(first_crossing_point, second_crossing_point), self.backpack_size):
             children_a.set_bit(index, parent_a.get_bits()[index])
             children_b.set_bit(index, parent_b.get_bits()[index])
         return children_a, children_b
 
     def mutation(self, mutation_probability):
-        for individual in self.__children:
+        for individual in self.children:
             if random.random() < mutation_probability:
                 individual.mutation()
 
@@ -85,53 +85,53 @@ class Population:
         summ = 0
 
         # We create sections for each individual in the population
-        for index, individual in enumerate(self.__individuals):
+        for index, individual in enumerate(self.individuals):
             sections.append([individual, summ, summ + individual.get_probability()])
             summ += individual.get_probability()
 
-        for _ in range(int(self.__size * 0.4)):  # we choose 40% parents by roulette selection
+        for _ in range(int(self.size * 0.45)):  # we choose 90% parents by roulette selection
             random_numer = random.uniform(0, 1)
             for section in sections:
                 if section[1] < random_numer <= section[2]:
-                    self.__parents.append(section[0])
+                    self.parents.append(section[0])
                     self.remove_individual(section[0])
                     sections.remove(section)
                     break
 
     def elite_selection(self):
-        self.__parents = []
-        for _ in range(int(self.__size * 0.1)):  # we choose 10% parents by elite_selection
+        self.parents = []
+        for _ in range(int(self.size * 0.05)):  # we choose 10% parents by elite_selection
             best_individual = self.get_best_individual()
-            self.__parents.append(best_individual)
+            self.parents.append(best_individual)
             self.remove_individual(best_individual)
 
     def choose_parents(self):
-        self.__parents = []
+        self.parents = []
         self.elite_selection()
         self.roulette_selection()
-        self.__individuals = self.__parents  # new population is made of selected parents and their children
+        self.individuals = self.parents  # new population is made of selected parents and their children
 
     def generate_children(self, crossover_probability, mutation_probability):
-        self.__children = []
+        self.children = []
 
-        for _ in range(len(self.__parents) // 2):
+        for _ in range(len(self.parents) // 2):
             # Use random.sample to pick two distinct parents
-            parent_indices = random.sample(range(len(self.__parents)), 2)
+            parent_indices = random.sample(range(len(self.parents)), 2)
 
-            parent_a = self.__parents[parent_indices[0]]
-            parent_b = self.__parents[parent_indices[1]]
+            parent_a = self.parents[parent_indices[0]]
+            parent_b = self.parents[parent_indices[1]]
 
             if random.random() < crossover_probability:
-                if random.random() < 0.7:  # setting single point crossing ratio to two point crossing
+                if random.random() < 0.70:  # setting single point crossing ratio to two point crossing
                     children_a, children_b = self.single_point_crossing(parent_a, parent_b)
                 else:
                     children_a, children_b = self.two_point_crossing(parent_a, parent_b)
             else:
                 children_a, children_b = parent_a, parent_b
 
-            self.__children.append(children_a)
-            self.__children.append(children_b)
+            self.children.append(children_a)
+            self.children.append(children_b)
 
         self.mutation(mutation_probability)
-        for children in self.__children:
-            self.__individuals.append(children)
+        for children in self.children:
+            self.individuals.append(children)
