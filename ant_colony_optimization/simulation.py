182d392f8972778613c760b53de8fdd4b9f5cce5
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-12-13 17:10:29 +0100

    Better graph and printing

--- ant_colony_optimization/simulation.py
+++ ant_colony_optimization/simulation.py
@@ -2,8 +2,10 @@ import sys
 
 from attraction import Attraction
 from colony import Colony
+from matplotlib.lines import Line2D
 import networkx as nx
 import matplotlib.pyplot as plt
+
 
 NR_ANTS = int(sys.argv[1])
 ALPHA = int(sys.argv[2])
@@ -38,17 +40,26 @@ def create_movement_graph(ant):
 
     pos = nx.get_node_attributes(G, 'pos')
     fig, ax = plt.subplots(figsize=(10, 8))
-    nx.draw(G, pos, ax=ax, with_labels=True, font_weight='bold', node_size=700, node_color='skyblue',
-            font_color='black')
+    nx.draw_networkx_nodes(G, pos, ax=ax, node_color='skyblue', node_size=700)
+    nx.draw_networkx_nodes(G, pos, nodelist=[ant.visited[0].id], node_color='green', node_size=700)
+    nx.draw_networkx_nodes(G, pos, nodelist=[ant.visited[-1].id], node_color='red', node_size=700)
+    nx.draw_networkx_edges(G, pos, ax=ax)
+    nx.draw_networkx_labels(G, pos, ax=ax, font_weight='bold', font_color='black')
 
     limits = plt.axis('on')
-    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+    ax.axis('off')
+
+    legend_elements = [Line2D([0], [0], marker='o', color='w', label='Początek', markerfacecolor='green', markersize=10),
+                       Line2D([0], [0], marker='o', color='w', label='Koniec', markerfacecolor='red', markersize=10),
+                       Line2D([0], [0], marker='o', color='w', label='Węzły odwiedzone po drodze', markerfacecolor='skyblue', markersize=10)]
+    ax.legend(handles=legend_elements, loc='best')
 
     plt.show()
 
 
 attractions = load_points_from_file(FILE_PATH)
 best_out_of_five = []
+total_distance = 0
 for round_number in range(5):
     best_ant = None
     for i in range(NR_ITERATIONS):
@@ -57,10 +68,14 @@ for round_number in range(5):
         ant_colony.update_pheromones()
         if best_ant is None or ant_colony.find_best_ant().distance < best_ant.distance:
             best_ant = ant_colony.find_best_ant()
-            print(f"Round {round_number + 1}, Iteration {i + 1}: Best ant's distance is {best_ant.distance}")
-    print(f"After round {round_number + 1}, number of ants: {len(ant_colony.ants)}")
+            print(f"Próba {round_number + 1}, Iteracja {i + 1}: Najkrótsza długość trasy: {best_ant.distance}")
+    print(f"Koniec próby {round_number + 1}")
     best_out_of_five.append(best_ant)
+    total_distance += best_ant.distance
+
+average_distance = total_distance / 5
+print(f"Średna długość tras mrówek po 5 próbach: {average_distance}")
 
 overall_best_ant = min(best_out_of_five, key=lambda ant: ant.distance)
 create_movement_graph(overall_best_ant)
-print(f"The overall best ant has a distance of {overall_best_ant.distance}")
+print(f"Najkrótsza długość trasy najlepszej mrówki z 5 prób: {overall_best_ant.distance}")
